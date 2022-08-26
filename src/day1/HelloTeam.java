4a41c86fe24767022f935d22c8c1543d4cdd36b4
Yusif <yusif.h@hotmail.com>
2022-08-25 23:26:33 -0400

    new class completed

--- src/day1/HelloTeam.java
+++ src/day1/HelloTeam.java
@@ -3,6 +3,8 @@ package day1;
 public class HelloTeam {
     public static void main(String[] args) {
         System.out.println("Hello Hello");
+        System.out.println("Do you see my new class in github??? :)");
+
 
     }
 }
