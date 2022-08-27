daf0dee08009890063200fb377aa9e39ab6cd0b7
shivaniharjani <shivanih06@gmail.com>
2022-08-27 16:47:41 -0400

    Updated new class

--- src/day3/Login3.java
+++ src/day3/Login3.java
@@ -4,8 +4,9 @@ public class Login3 {
     public static void main(String[] args) {
 
         System.out.println("Hey team");
+        System.out.println("Updating 'login3' class");
     }
 
     // created day3 package and new class
-
+// updating class
 }
