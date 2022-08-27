ebe979c21ac2f01c20ee9de91627c06b1f889cd4
shivaniharjani <shivanih06@gmail.com>
2022-08-27 16:46:55 -0400

    Created day3 pkg and new class

---
+++ src/day3/Login3.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class Login3 {
+    public static void main(String[] args) {
+
+        System.out.println("Hey team");
+    }
+
+    // created day3 package and new class
+
+}
